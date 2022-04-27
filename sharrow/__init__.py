2ccfb08ee1b38b88e8712e91e9c4f9eea4d155c5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-27 22:44:19 +0800

    Delete sharrow directory

--- sharrow/__init__.py
+++
@@ -1,9 +0,0 @@
-from xarray import DataArray
-
-from . import example_data, selectors, shared_memory
-from ._version import version as __version__
-from .dataset import Dataset
-from .digital_encoding import array_decode, array_encode
-from .flows import Flow
-from .relationships import DataTree, Relationship
-from .table import Table, concat_tables
