2986edc681fcb733681e19eb37d0ad8d781590a4
Jeff Newman <jeff@driftless.xyz>
2024-11-25 10:38:15 -0600

    ruffen

--- sharrow/tests/test_example_data.py
+++ sharrow/tests/test_example_data.py
@@ -1,6 +1,7 @@
-import sharrow as sh
 import numpy as np
 import pandas as pd
+
+import sharrow as sh
 
 
 def test_skims():
@@ -11,6 +12,7 @@ def test_skims():
         np.asarray([[0.12, 0.24, 0.44], [0.37, 0.14, 0.28]]),
     )
 
+
 def test_skims_zarr():
     skims = sh.example_data.get_skims_zarr()
     assert isinstance(skims, sh.Dataset)
@@ -18,6 +20,7 @@ def test_skims_zarr():
         skims.DIST.values[:2, :3],
         np.asarray([[0.12, 0.24, 0.44], [0.37, 0.14, 0.28]]),
     )
+
 
 def test_skims_omx():
     skims = sh.example_data.get_skims_omx()
@@ -27,20 +30,24 @@ def test_skims_omx():
         np.asarray([[0.12, 0.24, 0.44], [0.37, 0.14, 0.28]]),
     )
 
+
 def test_maz_to_taz():
     maz_to_taz = sh.example_data.get_maz_to_taz()
     assert isinstance(maz_to_taz, pd.DataFrame)
     assert maz_to_taz.index.name == "MAZ"
 
+
 def test_maz_to_maz_walk():
     maz_to_maz_walk = sh.example_data.get_maz_to_maz_walk()
     assert isinstance(maz_to_maz_walk, pd.DataFrame)
-    assert list(maz_to_maz_walk.columns) == ['OMAZ', 'DMAZ', 'DISTWALK']
+    assert list(maz_to_maz_walk.columns) == ["OMAZ", "DMAZ", "DISTWALK"]
+
 
 def test_land_use():
     land_use = sh.example_data.get_land_use()
     assert isinstance(land_use, pd.DataFrame)
     assert land_use.index.name == "TAZ"
+
 
 def test_data():
     data = sh.example_data.get_data()
